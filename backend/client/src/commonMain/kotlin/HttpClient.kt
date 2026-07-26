b1fa687c7734c6c7cb279830e30af0b1fb2fe8c1
bartoszciosek <ciosek000@gmail.com>
2026-07-26 14:57:05 +0200

    implement backend

---
+++ backend/client/src/commonMain/kotlin/HttpClient.kt
@@ -0,0 +1,9 @@
+package net.bartoszciosek
+
+import io.ktor.client.HttpClient
+import io.ktor.client.*
+import kotlinx.rpc.krpc.ktor.client.installKrpc
+
+val httpClient = HttpClient {
+    installKrpc()
+}
---
+++ backend/server/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kotlinx.rpc)
+}
+
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.cors)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.kotlinx.rpc.server)
+    implementation(libs.logback.classic)
+    implementation(project(":core"))
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+    testImplementation(libs.kotlinx.rpc.client)
+}
